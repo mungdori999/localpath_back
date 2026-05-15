cb3850060f89543a5ab1004b6eb9dea934e2ca74
mungdori <mungdori999@gmail.com>
2026-05-15 09:59:22 +0900

    first commit

---
+++ src/main/java/com/mungdori/localpath/LocalpathApplication.java
@@ -0,0 +1,13 @@
+package com.mungdori.localpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalpathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalpathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mungdori/localpath/LocalpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungdori.localpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalpathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
