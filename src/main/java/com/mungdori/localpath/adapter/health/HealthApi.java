681ab909573997f88719b977017636e0e84ecf71
mungdori <mungdori999@gmail.com>
2026-06-01 13:17:06 +0900

    health 체크 오류 수정

--- src/main/java/com/mungdori/localpath/adapter/health/HealthApi.java
+++ src/main/java/com/mungdori/localpath/adapter/health/HealthApi.java
@@ -3,9 +3,11 @@ package com.mungdori.localpath.adapter.health;
 import com.mungdori.localpath.common.constants.ApiPaths;
 import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(ApiPaths.API)
+@RestController
+@RequestMapping(ApiPaths.API)
 public class HealthApi {
 
     @GetMapping("/health")
